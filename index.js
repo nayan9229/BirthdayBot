cb1fee99b582765564e58cb1a66b0b96fc86e001
Nayan Mevada <nayanmevada@ymail.com>
2019-09-05 09:03:43 +0530

    Bug Fix

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
  */
 app.get('/webhook', function (req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
-        req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+        req.query['hub.verify_token'] === CONFIG.VERIFICATION_TOKEN) {
         console.log("Validating webhook");
         res.status(200).send(req.query['hub.challenge']);
     } else {
